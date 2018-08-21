2fd9ed03eb0edbd2e95b88ef2cfae112974d81f4
Matteo Pellegrino <matteo.pelle.pellegrino@gmail.com>
2018-08-21 22:44:02 +0200

    Improve sample app and add guidelines comments

--- sample/src/main/java/it/matteopellegrino/pagedgrid/sample/MainActivity.kt
+++ sample/src/main/java/it/matteopellegrino/pagedgrid/sample/MainActivity.kt
@@ -3,7 +3,11 @@ package it.matteopellegrino.pagedgrid.sample
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.Toast
+import it.matteopellegrino.pagedgrid.element.AbstractElement
 import it.matteopellegrino.pagedgrid.element.BitmapIcon
 import it.matteopellegrino.pagedgrid.element.DrawableIcon
 import it.matteopellegrino.pagedgrid.grid.EmptyGrid
@@ -15,21 +19,64 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val page1 = EmptyGrid(4, 4)
+        // Create a simple empty grid, with 4 columns and 5 rows. This will be the first page
+        val page1 = EmptyGrid(4, 5)
 
-        page1[1, 2] = DrawableIcon("(1,2)", R.drawable.ic_outline_assignment_24px)
-        page1[2, 3] = DrawableIcon("(2,3)", R.drawable.ic_outline_assignment_24px)
-        page1[3, 1] = DrawableIcon("(3,1)", R.drawable.ic_outline_assignment_24px)
+        // Fill the grid with drawables from resources.
+        // DrawableIcon is a ready to use layout to show an image with a title
+        page1[0, 0] = DrawableIcon("Activity", R.drawable.ic_format_align_justify_black_24dp)
+        page1[1, 0] = DrawableIcon("Airplay", R.drawable.ic_format_align_center_black_24dp)
+        page1[2, 0] = DrawableIcon("Award", R.drawable.ic_format_align_left_black_24dp)
+        page1[3, 0] = DrawableIcon("Chart", R.drawable.ic_format_align_right_black_24dp)
 
-        val page2 = EmptyGrid(3, 4)
-        page2[2, 2] = BitmapIcon("(3,2)", BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
+        page1[0, 2] = DrawableIcon("Photo Filter", R.drawable.ic_photo_filter_black_24dp)
+        page1[2, 2] = DrawableIcon("Portrait", R.drawable.ic_portrait_black_24dp)
 
-        val page3 = EmptyGrid(4, 3)
+        // Create a second page.
+        val page2 = EmptyGrid(2, 3)
 
+        // Prepare some resources to load as bitmaps.
+        // In a realistic case these should be load from internal storage or via network.
+        val imgs = arrayOf(
+                arrayOf(R.mipmap.img_1, R.mipmap.img_2, R.mipmap.img_3),
+                arrayOf(R.mipmap.img_4, R.mipmap.img_5, R.mipmap.img_6))
+
+        // Iterate over the empty grid of the second page.
+        // An helper method allows accessing the correct coordinates
+        page2.forEachIndexed { x, y, _ ->
+            // Assign the cell with coordinate (x, y) a new Element using the BitmapIcon implementation
+            page2[x, y] = BitmapIcon("($x, $y)", BitmapFactory.decodeResource(resources, imgs[x][y]))
+        }
+
+        val page3 = EmptyGrid(1, 3)
+
+        val cards = arrayOf(R.mipmap.img_7, R.mipmap.img_8, R.mipmap.img_9)
+
+        page3.forEachIndexed { x, y, _ ->
+
+            // For custom view you only need to implemente Element or extend AbstractElement (recommended)
+            // Here an example using an anonymous object to create a simple ImageView (avoiding the title mandatory in DrawableIcon/BitmapIcon)
+            page3[x, y] = object : AbstractElement() {
+                override fun inflateView(parent: ViewGroup): View {
+                    val img = ImageView(baseContext)
+                    img.scaleType = ImageView.ScaleType.CENTER_CROP
+                    img.setImageBitmap(BitmapFactory.decodeResource(resources, cards[y]))
+                    return img
+                }
+            }
+        }
+
+        // Effectively show the pages assigning them to the corresponding property of PagedGridView
+        // No notify is required. The adapter will update by itself.
         pagedGridView.pages = mutableListOf(page1, page2, page3)
 
+        // If you want some iteration with the user
+        // you can access to every view of each cell, but the empty ones.
         pagedGridView.pages.forEach { page ->
             page.forEachIndexed { x, y, elem ->
+
+                // This method offers by Element allows you accessing the view when inflating (created)
+                // in order to attach whatever android event to the view, such as a click listener
                 elem.setOnInflateViewListener { view ->
                     view.setOnClickListener {
                         Toast.makeText(baseContext, "View on ($x, $y) clicked", Toast.LENGTH_SHORT).show()
